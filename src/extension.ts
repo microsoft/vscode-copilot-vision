6b1212475ed65c7be16e641a7dc9adfb8d9647b5
meganrogge <merogge@microsoft.com>
2024-10-08 09:38:40 -0700

    rm submit

--- src/extension.ts
+++ src/extension.ts
@@ -80,7 +80,6 @@ export function activate(context: vscode.ExtensionContext) {
                 }]
             };
             await vscode.commands.executeCommand('workbench.action.chat.open', options);
-            await vscode.commands.executeCommand('workbench.action.chat.submit');
         } else {
             console.error('Failed to take screenshot.');
         }
