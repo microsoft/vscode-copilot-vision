5c9fd5e89b2304be520b882fcae707e9040b93f5
Daniel Imms <2193314+Tyriar@users.noreply.github.com>
2024-10-10 10:59:13 -0700

    Format to use tabs consistently

--- src/extension.ts
+++ src/extension.ts
@@ -2,8 +2,8 @@ import axios from 'axios';
 import * as dotenv from 'dotenv';
 import * as vscode from 'vscode';
 import { ChatVariablesCollection } from './chatVariablesCollective';
-import { AzureOpenAI } from "openai";  
-import { DefaultAzureCredential } from "@azure/identity";  
+import { AzureOpenAI } from "openai";
+import { DefaultAzureCredential } from "@azure/identity";
 import { Models } from 'openai/resources/models.mjs';
 
 dotenv.config();
@@ -11,8 +11,8 @@ dotenv.config();
 const VISION_PARTICIPANT_ID = 'chat-sample.vision';
 
 // Azure OpenAI credentials
-const endpoint = process.env["AZURE_ENDPOINT"] || "https://vscode-openai.openai.azure.com/";  
-const apiVersion = "2024-05-01-preview";  
+const endpoint = process.env["AZURE_ENDPOINT"] || "https://vscode-openai.openai.azure.com/";
+const apiVersion = "2024-05-01-preview";
 const deployment = "gpt-4o-mini"; // This must match your deployment name
 const AZURE_API_KEY = process.env["AZURE_API_KEY"];
 
@@ -23,9 +23,9 @@ const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 let cachedToken: string | undefined;
 
 interface IVisionChatResult extends vscode.ChatResult {
-    metadata: {
-        command: string;
-    }
+	metadata: {
+		command: string;
+	}
 }
 
 // Use gpt-4o since it is fast and high quality. gpt-3.5-turbo and gpt-4 are also available.
@@ -33,39 +33,39 @@ const MODEL_SELECTOR: vscode.LanguageModelChatSelector = { vendor: 'copilot', fa
 
 export function activate(context: vscode.ExtensionContext) {
 
-	
-	  const disposable = vscode.commands.registerCommand('extension.showHtmlPreview', () => {
-        const panel = vscode.window.createWebviewPanel(
-            'htmlPreview', // Identifies the type of the webview. Used internally
-            'HTML Preview', // Title of the panel displayed to the user
-            vscode.ViewColumn.One, // Editor column to show the new webview panel in
-            {
-                enableScripts: true // Enable scripts in the webview
-            }
-        );
 
-        // Set the HTML content for the webview
-      	const editor = vscode.window.activeTextEditor;
+	const disposable = vscode.commands.registerCommand('extension.showHtmlPreview', () => {
+		const panel = vscode.window.createWebviewPanel(
+			'htmlPreview', // Identifies the type of the webview. Used internally
+			'HTML Preview', // Title of the panel displayed to the user
+			vscode.ViewColumn.One, // Editor column to show the new webview panel in
+			{
+				enableScripts: true // Enable scripts in the webview
+			}
+		);
+
+		// Set the HTML content for the webview
+		const editor = vscode.window.activeTextEditor;
 		if (editor) {
 			const htmlContent = editor.document.getText();
 			panel.webview.html = getWebviewContent(htmlContent);
 		} else {
 			vscode.window.showErrorMessage('No active text editor found.');
 		}
-    });
+	});
 
-    context.subscriptions.push(disposable);
+	context.subscriptions.push(disposable);
 
-    // Define a chat handler
-    const handler: vscode.ChatRequestHandler = async (request: vscode.ChatRequest, context: vscode.ChatContext, stream: vscode.ChatResponseStream, token: vscode.CancellationToken): Promise<IVisionChatResult> => {
-        // To talk to an LLM in your subcommand handler implementation, your
-        // extension can use VS Code's `requestChatAccess` API to access the Copilot API.
-        // The GitHub Copilot Chat extension implements this provider.
+	// Define a chat handler
+	const handler: vscode.ChatRequestHandler = async (request: vscode.ChatRequest, context: vscode.ChatContext, stream: vscode.ChatResponseStream, token: vscode.CancellationToken): Promise<IVisionChatResult> => {
+		// To talk to an LLM in your subcommand handler implementation, your
+		// extension can use VS Code's `requestChatAccess` API to access the Copilot API.
+		// The GitHub Copilot Chat extension implements this provider.
 
-        // This just converts our sources/references into more digestible format. Helpful for more complex variables.
+		// This just converts our sources/references into more digestible format. Helpful for more complex variables.
 		// const chatVariables = new ChatVariablesCollection(request.references);;
 
-        const chatVariables = request.references
+		const chatVariables = request.references
 		stream.progress('Sending request to OpenAI...');
 
 		if (chatVariables.length === 0) {
@@ -74,45 +74,45 @@ export function activate(context: vscode.ExtensionContext) {
 		}
 
 		let base64String = '';
-        let mimeType = 'image/png';
+		let mimeType = 'image/png';
 		const content: Array<{ type: 'text', text: string } | { type: 'image_url', image_url: { url: string, detail?: string } }> = [
-				{ type: 'text', text: request.prompt },
-			];
+			{ type: 'text', text: request.prompt },
+		];
 
-			for (const reference of chatVariables) {
-				// URI in cases of drag and drop or from file already in the workspace
-				if (reference.value instanceof vscode.Uri) {
-					const fileExtension = reference.value.path.split('.').pop()?.toLowerCase();
-					const imageExtensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff'];
+		for (const reference of chatVariables) {
+			// URI in cases of drag and drop or from file already in the workspace
+			if (reference.value instanceof vscode.Uri) {
+				const fileExtension = reference.value.path.split('.').pop()?.toLowerCase();
+				const imageExtensions = ['jpg', 'jpeg', 'png', 'gif', 'bmp', 'tiff'];
 
-					if (fileExtension && imageExtensions.includes(fileExtension)) {
-						const fileData = await vscode.workspace.fs.readFile(reference.value);
-						base64String = Buffer.from(fileData).toString('base64');
-                        content.push({ type: 'image_url', image_url: { url: `data:${mimeType};base64,${base64String}`} });
-					} else {
-						stream.markdown(`The file is not an image.`);
-						return { metadata: { command: '' } };
-					}
+				if (fileExtension && imageExtensions.includes(fileExtension)) {
+					const fileData = await vscode.workspace.fs.readFile(reference.value);
+					base64String = Buffer.from(fileData).toString('base64');
+					content.push({ type: 'image_url', image_url: { url: `data:${mimeType};base64,${base64String}` } });
+				} else {
+					stream.markdown(`The file is not an image.`);
+					return { metadata: { command: '' } };
+				}
 
 				// ChatReferenceBinaryData in cases of copy and paste (or from quick pick)
-                } else if (reference.value instanceof vscode.ChatReferenceBinaryData) {
-                    mimeType = reference.value.mimeType;
-                    const buffer = await reference.value.data();
-                    base64String = Buffer.from(buffer).toString('base64');
-                    content.push({ type: 'image_url', image_url: { url: `data:${mimeType};base64,${base64String}`} });
-                }
+			} else if (reference.value instanceof vscode.ChatReferenceBinaryData) {
+				mimeType = reference.value.mimeType;
+				const buffer = await reference.value.data();
+				base64String = Buffer.from(buffer).toString('base64');
+				content.push({ type: 'image_url', image_url: { url: `data:${mimeType};base64,${base64String}` } });
+			}
+		}
+
+		try {
+			const token = await getOpenAiApiToken();
+			if (token === undefined) {
+				stream.markdown('Please provide a valid Open AI token.');
+				return { metadata: { command: '' } };
 			}
 
-		try {	
-            const token = await getOpenAiApiToken();
-            if (token === undefined) {
-                stream.markdown('Please provide a valid Open AI token.');
-                return { metadata: { command: '' } };
-            }
-            
 			const openAIRequest = {
 				model: 'gpt-4o',
-				messages: [ { role: 'user', content } ]
+				messages: [{ role: 'user', content }]
 			};
 
 			// Send the request to OpenAI
@@ -129,7 +129,7 @@ export function activate(context: vscode.ExtensionContext) {
 
 			// // Initialize the AzureOpenAI client with Entra ID (Azure AD) authentication
 			// const client = new AzureOpenAI({ endpoint, apiVersion, deployment, apiKey: AZURE_API_KEY});  
-		
+
 			// // EXAMPLE OF USING AZURE OPENAI
 			// const result = await client.chat.completions.create({
 			// 	messages: [
@@ -149,104 +149,104 @@ export function activate(context: vscode.ExtensionContext) {
 			// 		stream.markdown(choice.message.content);
 			// 	}
 			// }	
-			
-	
+
+
 		} catch (err: unknown) {
-            // Invalidate token if it's a 401 error
-            if (typeof err === 'object' && err && 'status' in err && err.status === 401) {
-                cachedToken = undefined;
-            }
+			// Invalidate token if it's a 401 error
+			if (typeof err === 'object' && err && 'status' in err && err.status === 401) {
+				cachedToken = undefined;
+			}
 			handleError(logger, err, stream);
 		}
 
 		return { metadata: { command: '' } };
-    };
+	};
 
 	const vision = vscode.chat.createChatParticipant(VISION_PARTICIPANT_ID, handler);
-    vision.iconPath = vscode.Uri.joinPath(context.extensionUri, 'vscode-logo.png');
+	vision.iconPath = vscode.Uri.joinPath(context.extensionUri, 'vscode-logo.png');
 
-    const logger = vscode.env.createTelemetryLogger({
-        sendEventData(eventName, data) {
-            // Capture event telemetry
-            console.log(`Event: ${eventName}`);
-            console.log(`Data: ${JSON.stringify(data)}`);
-        },
-        sendErrorData(error, data) {
-            // Capture error telemetry
-            console.error(`Error: ${error}`);
-            console.error(`Data: ${JSON.stringify(data)}`);
-        }
-    });
+	const logger = vscode.env.createTelemetryLogger({
+		sendEventData(eventName, data) {
+			// Capture event telemetry
+			console.log(`Event: ${eventName}`);
+			console.log(`Data: ${JSON.stringify(data)}`);
+		},
+		sendErrorData(error, data) {
+			// Capture error telemetry
+			console.error(`Error: ${error}`);
+			console.error(`Data: ${JSON.stringify(data)}`);
+		}
+	});
 
-    context.subscriptions.push(vision.onDidReceiveFeedback((feedback: vscode.ChatResultFeedback) => {
-        // Log chat result feedback to be able to compute the success matric of the participant
-        // unhelpful / totalRequests is a good success metric
-        logger.logUsage('chatResultFeedback', {
-            kind: feedback.kind
-        });
-    }));
+	context.subscriptions.push(vision.onDidReceiveFeedback((feedback: vscode.ChatResultFeedback) => {
+		// Log chat result feedback to be able to compute the success matric of the participant
+		// unhelpful / totalRequests is a good success metric
+		logger.logUsage('chatResultFeedback', {
+			kind: feedback.kind
+		});
+	}));
 }
 
 async function getOpenAiApiToken(): Promise<string | undefined> {
-    // Return cached token if available
-    if (cachedToken) {
-        return cachedToken;
-    }
+	// Return cached token if available
+	if (cachedToken) {
+		return cachedToken;
+	}
 
-    // Pick up environment variable (mostly for development)
-    if (OPENAI_API_KEY) {
-        return OPENAI_API_KEY;
-    }
-    
-    // Get from simple input box
-    const inputBox = vscode.window.createInputBox();
-    inputBox.title = 'Enter Azure OpenAI API Key';
-    const disposables: vscode.Disposable[] = [];
-    const value = new Promise<string | undefined>(r => {
-        inputBox.onDidTriggerButton(e => {
-        });
-        disposables.push(inputBox.onDidAccept(() => {
-            inputBox.hide();
-            r(inputBox.value);
-        }));
-        disposables.push(inputBox.onDidHide(() => {
-            r(undefined);
-        }));
-    });
-    inputBox.show();
+	// Pick up environment variable (mostly for development)
+	if (OPENAI_API_KEY) {
+		return OPENAI_API_KEY;
+	}
 
-    cachedToken = await value;
-    if (!cachedToken) { // Normalize
-        cachedToken = undefined;
-    }
+	// Get from simple input box
+	const inputBox = vscode.window.createInputBox();
+	inputBox.title = 'Enter Azure OpenAI API Key';
+	const disposables: vscode.Disposable[] = [];
+	const value = new Promise<string | undefined>(r => {
+		inputBox.onDidTriggerButton(e => {
+		});
+		disposables.push(inputBox.onDidAccept(() => {
+			inputBox.hide();
+			r(inputBox.value);
+		}));
+		disposables.push(inputBox.onDidHide(() => {
+			r(undefined);
+		}));
+	});
+	inputBox.show();
 
-    for (const d of disposables) {
-        d.dispose();
-    }
+	cachedToken = await value;
+	if (!cachedToken) { // Normalize
+		cachedToken = undefined;
+	}
 
-    return cachedToken;
+	for (const d of disposables) {
+		d.dispose();
+	}
+
+	return cachedToken;
 }
 
 function handleError(logger: vscode.TelemetryLogger, err: any, stream: vscode.ChatResponseStream): void {
-    // making the chat request might fail because
-    // - model does not exist
-    // - user consent not given
-    // - quote limits exceeded
-    logger.logError(err);
-    
-    if (err instanceof vscode.LanguageModelError) {
-        console.log(err.message, err.code, err.cause);
-        if (err.cause instanceof Error && err.cause.message.includes('off_topic')) {
-            stream.markdown(vscode.l10n.t('I\'m sorry, I can only explain computer science concepts.'));
-        }
-    } else {
-        // re-throw other errors so they show up in the UI
-        throw err;
-    }
+	// making the chat request might fail because
+	// - model does not exist
+	// - user consent not given
+	// - quote limits exceeded
+	logger.logError(err);
+
+	if (err instanceof vscode.LanguageModelError) {
+		console.log(err.message, err.code, err.cause);
+		if (err.cause instanceof Error && err.cause.message.includes('off_topic')) {
+			stream.markdown(vscode.l10n.t('I\'m sorry, I can only explain computer science concepts.'));
+		}
+	} else {
+		// re-throw other errors so they show up in the UI
+		throw err;
+	}
 }
 
 function getWebviewContent(htmlContent: string): string {
-    return `
+	return `
         <!DOCTYPE html>
         <html lang="en">
         <head>
