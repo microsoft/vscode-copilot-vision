33f20f726b791f713d4b4cfd2e7f2bf46fc1a757
meganrogge <merogge@microsoft.com>
2024-10-16 10:45:05 -0700

    better name

--- src/imageUtils.ts
+++ src/imageUtils.ts
@@ -2,7 +2,7 @@
 // matches images in markdown, html, and markdown links when they do not have alt text
 const imageRegex = /!\[\s*\]\(([^)]+)\)|<img\s+[^>]*src="([^"]+)"[^>]*>|\[!\[\s*\]\(([^)]+)\)\]\(([^)]+)\)/;
 
-export function parseLine(line: string): { imagePath: string, altTextStartIndex: number, isHTML?: boolean } | undefined {
+export function extractImageInfo(line: string): { imagePath: string, altTextStartIndex: number, isHTML?: boolean } | undefined {
 	const match = line.match(imageRegex);
 	if (!match || match.index === undefined) {
 		return;
@@ -10,7 +10,7 @@ export function parseLine(line: string): { imagePath: string, altTextStartIndex:
 	let altTextStartIndex = 1;
 	let expectedIndex = 1;
 	let isHTML = false;
-	if (match[0].startsWith('\<')) {
+	if (match[0].startsWith('<')) {
 		expectedIndex = 2;
 		altTextStartIndex = 1;
 		isHTML = true;
--- src/test/altTextGeneratorQuickFix.test.ts
+++ src/test/altTextGeneratorQuickFix.test.ts
@@ -1,18 +1,18 @@
 import assert from 'assert';
 import { suite, test } from 'mocha';
-import { parseLine } from '../imageUtils';
+import { extractImageInfo } from '../imageUtils';
 
 suite('imageRegex', () => {
 	test('should capture the image path in markdown image syntax', () => {
 		const markdownImage = '![](path/to/image.png)';
-		const match = parseLine(markdownImage);
+		const match = extractImageInfo(markdownImage);
 		assert(match);
 		assert.equal(match.imagePath, 'path/to/image.png');
 	});
 
 	test('should capture the image path in HTML image syntax', () => {
 		const htmlImage = '<img src="path/to/image.png" />';
-		const match = parseLine(htmlImage);
+		const match = extractImageInfo(htmlImage);
 		assert(match);
 		assert.equal(match.imagePath, 'path/to/image.png');
 		assert.equal(match.altTextStartIndex, 1);
@@ -21,7 +21,7 @@ suite('imageRegex', () => {
 
 	test('should capture the image path in markdown link with image syntax', () => {
 		const markdownLinkImage = '[![](path/to/image.png)](http://example.com)';
-		const match = parseLine(markdownLinkImage);
+		const match = extractImageInfo(markdownLinkImage);
 		assert(match);
 		assert.equal(match.imagePath, 'path/to/image.png');
 		assert.equal(match.altTextStartIndex, 3);
@@ -29,13 +29,13 @@ suite('imageRegex', () => {
 
 	test('should not match if there is alt text in markdown image syntax', () => {
 		const markdownImageWithAlt = '![alt text](path/to/image.png)';
-		const match = parseLine(markdownImageWithAlt);
+		const match = extractImageInfo(markdownImageWithAlt);
 		assert(!match);
 	});
 
 	test('should not match if there is alt text in markdown link with image syntax', () => {
 		const markdownLinkImageWithAlt = '[![alt text](path/to/image.png)](http://example.com)';
-		const match = parseLine(markdownLinkImageWithAlt)
+		const match = extractImageInfo(markdownLinkImageWithAlt)
 		assert(!match);
 	});
 });
